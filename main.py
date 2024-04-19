2c1a4b1400a2429cccaf39293dd8f960712f3956
Anton Kokarev <net-files@yandex.ru>
2024-04-19 09:40:53 +0300

    Дата из сообщения. Обрезка рублей на конце стоимости

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import Update
 import logging
 import datetime
+import pytz
 import json
 
 # Загрузка настроек
@@ -18,24 +19,42 @@ client = gspread.authorize(credentials)
 # Открытие таблицы по URL
 sheet = client.open_by_url(settings['spreadsheet_url']).worksheet(settings['worksheet'])
 
+
+#Функция обрезки лишних символов на конце строки
+def cutlast(s, tail):
+    if s[-len(tail):] == tail:
+        s = s[:-len(tail)] 
+    return s
+
 # Функция для обработки команды /start
 def start(update, context):
     update.message.reply_text('Привет! Отправь мне данные о пройденном километраже, имени события и его стоимости.')
 
 # Функция для сохранения данных в Google таблице
 def save_data(update, context):
+    chat_id = update.message.chat.id
+    date_msg_utc = update.message.forward_date if update.message.forward_date else update.message.date
+    tz = pytz.timezone('Europe/Moscow')
+    date_msg = date_msg_utc.astimezone(tz)
+    
     data = update.message.text.split()
     cost = data.pop(-1)
+    cost = cutlast(cost, 'руб.')
+    cost = cutlast(cost, 'руб')
+    cost = cutlast(cost, 'р.')
+    cost = cutlast(cost, 'р')
+
     if data[0].isnumeric():
         kilometers = data.pop(0)
     else:
         kilometers = ''
 
     event = ' '.join(data)
-    time = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
+    time = date_msg.strftime("%Y-%m-%d-%H:%M")
 
     if cost.isnumeric() and event:
         row = [time, kilometers, event, cost]
+        print(row)
         sheet.append_row(row)
         update.message.reply_text('Данные успешно сохранены в Google таблице.')
     else:
